82add5c9a8dca66a76f31872e2b6fab952371542
Salvador Fandino <sfandino@yahoo.com>
2011-02-17 22:03:43 +0100

    export subs

--- lib/Math/GMPn.pm
+++ lib/Math/GMPn.pm
@@ -7,34 +7,26 @@ use warnings;
 require Exporter;
 
 our @ISA = qw(Exporter);
+our @EXPORT_OK = qw( mpn_add
+		     mpn_sub
+		     mpn_mul
+		     mpn_sqr
+		     mpn_mul_ext
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Math::GMPn ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
+		     mpn_get_str
+		     mpn_get_strp
+		     mpn_set_str
+		     mpn_set_strp
+		  );
+our @EXPORT = @EXPORT_OK; # remove me!
 
 our $VERSION = '0.01';
 
 require XSLoader;
 XSLoader::load('Math::GMPn', $VERSION);
 
-# Preloaded methods go here.
-
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
