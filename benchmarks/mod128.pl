87b25a1494d983647e7f79a02ef7c082b322d5a7
Salvador <sfandino@yahoo.com>
2011-02-21 18:48:01 +0100

    reimport Math::Int128 tests

--- benchmarks/mod128.pl
+++ benchmarks/mod128.pl
@@ -1,9 +1,25 @@
 #! perl -slw
 use strict;
 use Benchmark qw[ cmpthese ];
+use Math::Int128 qw[ uint128 uint128_to_hex :op ];
 use Math::GMPn;
 use Math::GMPz qw[ Rmpz_init_set_str ];
 
+sub FNV_1_128 {
+    my $s = shift;
+    my $h = uint128( '144066263297769815596495629667062367629' );
+    my $p = uint128( '309485009821345068724781371' );
+    $h *= $p, $h ^= $_ for unpack 'C*', $s;
+    return $h;
+}
+
+sub FNV_1a_128 {
+    my $s = shift;
+    my $h = uint128( '144066263297769815596495629667062367629' );
+    my $p = uint128( '309485009821345068724781371' );
+    $h ^= $_, $h *= $p for unpack 'C*', $s;
+    return $h;
+}
 
 sub FNV_1_128_gmpn {
     my $s = shift;
@@ -12,7 +28,7 @@ sub FNV_1_128_gmpn {
     for (unpack 'C*', $s) {
         my $h2 = $h;
         mpn_mul($h, $h2, $p);
-        mpn_or_int($h, $h, $_);
+        mpn_ior_uint($h, $h, $_);
     }
     return $h;
 }
@@ -22,7 +38,7 @@ sub FNV_1a_128_gmpn {
     mpn_set_str(my $h, '144066263297769815596495629667062367629', 10, 128);
     mpn_set_str(my $p, '309485009821345068724781371', 10, 128 );
     for (unpack 'C*', $s) {
-        mpn_or_int($h, $h, $_);
+        mpn_ior_uint($h, $h, $_);
         my $h2 = $h;
         mpn_mul($h, $h2, $p);
     }
@@ -104,6 +120,10 @@ our $text = do{ local( @ARGV, $/ ) = $0; <> };
 print length $text;
 
 cmpthese -1, {
+    int128 => q[
+        my $fnv1  = uint128_to_hex( FNV_1_128( $text ) );
+        my $fnv1a = uint128_to_hex( FNV_1a_128( $text ) );
+    ],
     GMPn => q[
         my $fnv1  = mpn_get_str(FNV_1_128_gmpn( $text ), 16);
         my $fnv1a = mpn_get_str(FNV_1a_128_gmpn( $text ), 16);
@@ -121,16 +141,21 @@ cmpthese -1, {
         my $fnv1a = Math::GMPz::Rmpz_get_str( FNV_1a_128_gmpz3( $text ), 16 );
     ],
     GMPz4 => q[
-        my $fnv1  = Math::GMPz::Rmpz_get_str( FNV_1_128_gmpz3( $text ), 16 );
-        my $fnv1a = Math::GMPz::Rmpz_get_str( FNV_1a_128_gmpz3( $text ), 16 );
+        my $fnv1  = Math::GMPz::Rmpz_get_str( FNV_1_128_gmpz4( $text ), 16 );
+        my $fnv1a = Math::GMPz::Rmpz_get_str( FNV_1a_128_gmpz4( $text ), 16 );
     ],
 };
 
-__END__
-C:\test>FNV128.pl
-2455
-         Rate   GMPz  GMPz2 int128
-GMPz   14.1/s     --   -77%   -95%
-GMPz2  61.4/s   334%     --   -78%
-int128  274/s  1840%   347%     --
+use Data::Dumper;
 
+print Dumper [ uint128_to_hex( FNV_1_128( $text ) ),
+               uint128_to_hex( FNV_1a_128( $text ) ),
+               Math::GMPz::Rmpz_get_str( FNV_1_128_gmpz( $text ), 16 ),
+               Math::GMPz::Rmpz_get_str( FNV_1a_128_gmpz( $text ), 16 ),
+               Math::GMPz::Rmpz_get_str( FNV_1_128_gmpz2( $text ), 16 ),
+               Math::GMPz::Rmpz_get_str( FNV_1a_128_gmpz2( $text ), 16 ),
+               Math::GMPz::Rmpz_get_str( FNV_1_128_gmpz3( $text ), 16 ),
+               Math::GMPz::Rmpz_get_str( FNV_1a_128_gmpz3( $text ), 16 ),
+               #Math::GMPz::Rmpz_get_str( FNV_1_128_gmpz4( $text ), 16 ),
+               #Math::GMPz::Rmpz_get_str( FNV_1a_128_gmpz4( $text ), 16 ) ];
+               ];
