1386ad5148736f1954183853828d9b5fb92d17d0
Salvador <sfandino@yahoo.com>
2011-02-22 19:05:26 +0100

    documented

--- lib/Math/GMPn.pm
+++ lib/Math/GMPn.pm
@@ -15,7 +15,6 @@ our @EXPORT = qw( GMP_LIMB_BITS
                   mpn_sub
                   mpn_mul
                   mpn_sqr
-                  mpn_emul
                   mpn_divrem
                   mpn_addmul
                   mpn_submul
@@ -110,12 +109,19 @@ Math::GMPn - Fixed length integer arithmetic.
 
 This module provides a set of functions to perform arithmetic on fixed
 length but arbitrarily large bit strings implemented on top of the GMP
-library.
+library low level functions (see
+L<http://gmplib.org/manual/Low_002dlevel-Functions.html>).
 
-The bit length of the strings passed to the module must be a
-multiple of the native machine integer size (i.e. 32 or 64 bits). Most
-operations do not check that condition and results are unspecified
-when other sizes are used.
+Numbers are represented as arrays of GMP mp_limb_t integers (usually,
+the native unsigned int) packed into Perl scalars without any
+additional wrapping.
+
+The bit length of the strings passed to the module must be a multiple
+of the mp_limb_t bit size (32 and 64 bits for 32bit and 64bit machines
+respectively). Most operations do not check that condition and their
+results are unspecified when non conforming sizes are used.
+
+Also, the strings passed must not b
 
 When strings of different length are used on the same operation, the
 result lenght is equal to that of the largest input. For instance,
@@ -126,7 +132,140 @@ string.
 
 =over 4
 
-=item mpn_set_str($to, $str, $base, $bitlen)
+=item GMP_LIMB_BYTES()
+
+Return the size in bytes of the mp_limb_t type used internally by GMP to
+represent numbers. It is 4 for 32bit machines and 8 for 64bit ones.
+
+=item GMP_LIMB_BITS()
+
+Return the size in bits of the mp_limb_t type. 32 or 64 for 32bit and
+64 bit machines respectively.
+
+=item mpn_set_str($r, $str, $base, $bitlen)
+
+Converts an ASCII representation of the number in the given base
+C<$base> to the internal representation used by Math::GMPn.
+
+Digits must be in the range '0'-'9' and 'a'-'z' or 'A'-'Z'.
+
+C<$base> must be between 2 and 36 (inclusive) and defaults to 10.
+
+If C<$bitlen> is not given, the minimum plausible bit length able to
+store the given number is used.
+
+=item mpn_set_str0($r, $str, $base, $bitlen)
+
+Converts a byte representation of the number in the given base
+C<$base> to the internal representation.
+
+For instance, the following calls are equivalent:
+
+  mpn_set_str0($a1, "\xf0\xaa\x01", 16, 128);
+  mpn_set_str0($a2, "f0aa01", 16, 128);
+
+=item mpn_set_random($r, $bitlen)
+
+Generate a random number of length <$bitlen>. The most significant
+limb is always non-zero.
+
+=item mpn_set_uint($r, $u1, $bitlen)
+
+Converts a Perl native number to Math::GMPn internal format.
+
+=item mpn_setior_uint($r, $u1, $offset, $bitlen)
+
+Sets the 
+
+=item mpn_set_bitlen($r, $bitlen, sign_extend)
+
+=item mpn_get_str($s1, $base)
+
+=item mpn_get_str0($s1, $base)
+
+=item mpn_get_uint($s1, $offset)
+
+=item mpn_ior($r, $s1, $s2)
+
+=item mpn_xor($r, $s1, $s2)
+
+=item mpn_and($r, $s1, $s2)
+
+=item mpn_andn($r, $s1, $s2)
+
+=item mpn_iorn($r, $s1, $s2)
+
+=item mpn_nand($r, $s1, $s2)
+
+=item mpn_nior($r, $s1, $s2)
+
+=item mpn_xnor($r, $s1, $s2)
+
+=item mpn_add($r, $s1, $s2)
+
+=item mpn_sub($r, $s1, $s2)
+
+=item mpn_mul($r, $s1, $s2)
+
+=item mpn_mul_ext($r, $s1, $s2)
+
+=item mpn_sqr($r, $s1)
+
+=item mpn_sqr_ext($r, $s1)
+
+=item mpn_divrem($q, $r, $s1, $s2)
+
+=item mpn_divexact_by3($r, $s)
+
+=item mpn_addmul($r, $s1, $s2)
+
+=item mpn_submul($r, $s1, $s2)
+
+=item mpn_lshift($r, $u1)
+
+=item mpn_rshift($r, $u1)
+
+=item $cnt = mpn_popcount($s1)
+
+=item $cnt = mpn_hamdist($s1, $s2)
+
+=item $pos = mpn_scan0($s1, $start)
+
+=item $pos = mpn_scan1($s1, $start)
+
+=item $cmp = mpn_cmp($s1, $s2)
+
+=item $bool = mpn_perfect_square_p($s1)
+
+=item $mpn_gcd_dest($r, $sd1, $sd2)
+
+=item mpn_ior_uint($r, $s1, $u1)
+
+=item mpn_xor_uint($r, $s1, $u1)
+
+=item mpn_and_uint($r, $s1, $u1)
+
+=item mpn_andn_uint($r, $s1, $u1)
+
+=item mpn_iorn_uint($r, $s1, $u1)
+
+=item mpn_nand_uint($r, $s1, $u1)
+
+=item mpn_nior_uint($r, $s1, $u1)
+
+=item mpn_xnor_uint($r, $s1, $u1)
+
+=item mpn_add_uint($r, $s1, $u1)
+
+=item mpn_sub_uint($r, $s1, $u1)
+
+=item mpn_mod_uint($r, $s1, $u1)
+
+=item mpn_mul_uint($r, $s1, $u1)
+
+=item mpn_addmul_uint($r, $s1, $u1)
+
+=item mpn_submul_uint($r, $s1, $u1)
 
 =back
 
